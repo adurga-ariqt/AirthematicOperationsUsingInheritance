d59bae3e32b96b6d2d1fb65298e7c8c3dcecd02c
adurga-ariqt <adurga@ariqt.com>
2022-11-18 15:59:45 +0530

    Add project files.

---
+++ AirthematicOperationsUsingInheritance/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AirthematicOperationsUsingInheritance;
+using MyClassLibrary3;
+
+Console.WriteLine("Hello, World!");
+Substraction obj = new Substraction(3,5 ,8);
+obj.MyMethod3();
+
---
+++ AirthematicOperationsUsingInheritance/Substraction.cs
@@ -0,0 +1,24 @@
+﻿using MyClassLibrary3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirthematicOperationsUsingInheritance
+{
+    public  class Substraction: Multiplication
+    {public double num4 { get; set; }
+        public Substraction(int a,int b ,int c):base(a,b)
+        {
+            num3 = c;
+        }
+        public void MyMethod3()
+        {
+            MyMethod2();
+            num4=num1-num3 -num2;
+            Console.WriteLine("the substraction of three numbers :" + num4); 
+        }
+
+    }
+}
